1bac7515dbf5b29d7d9f5d39a322eb8eff0431db
ParksoulSliptick <163039921+ParksoulSliptick@users.noreply.github.com>
2024-09-11 13:07:42 -0600

    Aprendimos a crear js

---
+++ 01-Fundamentos de Programacion/comentarios.js
@@ -0,0 +1,21 @@
+//Comentarios
+
+/*
+Comentaros en bloque
+
+Para mas de una linea de cometarios.
+Usamos el shortcut shift + alt + a
+*/
+
+
+// La funcion alert() lanza una ventana de alerta
+alert("Estoy enlazado!!");
+/*
+Recomendaciones
+
+
+1. No debemos recargar mucho mucho nuestro codgo de comentarios 
+2. Los comentarios deben ser concretos y descriptivos.
+3. No deben 
+
+*/
---
+++ 01-Fundamentos de Programacion/variables.js
@@ -0,0 +1,35 @@
+/*
+No son mas que contenedores que almacenan datos
+
+Las variables se declaran usando las palabras reservadas
+
+var: Es la forma tradicional pero ya no es recomendada
+let: Es la forma moderna, es la recomendada
+const: El dato almacenado no puede cambiar
+
+Para declarar una variable:
+
+var nambre variable: valor;
+
+*/
+
+nombre = "Alejandro";
+edad = 20;
+vivo = true;
+
+console.log(nombre);
+console.log(edad);
+console.log(vivo);
+
+
+/*
+Las variables declaradas con "var" y "let" pueden cambiar a lo largo del codigo
+siempre y cuando su alcance lo permite
+
+Las variables declaradas con "const" no pueden cambiar.
+Si lo intentamos marcara un error
+*/
+
+nombre = "Puchungito"
+
+console.log(nombre);
